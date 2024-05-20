4b81c5adbb0c209e4832a927542a03829f610ca3
Wamiq Raza <sugh101@gmail.com>
2024-05-20 12:26:36 +0200

    users

---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from app.db.db import Base
+
+class User():
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
